113ef9778363724abe313cb747e4ed6afcc8fa52
lyyyop <lyyyop@qq.com>
2021-12-27 03:20:16 -0800

    实现注册

--- main.js
+++ main.js
@@ -2,9 +2,30 @@ const express=require('express')
 const mongoose = require('mongoose');
 const app = express();
 const ejs=require('ejs')
-
-//mongoose.connect('mongodb://172.21.2.236:27017/190110910232');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910232');
+const UserSchema = {
+    name: String,
+    password: String
+};
+const User = mongoose.model("User" , UserSchema);
+const BookSchema = {
+    author: String,
+    title: String,
+    place: String,
+    price: Number
+  };
+const Book = mongoose.model("Book" , BookSchema);
 
 app.use('/',express.static('public'))
 
+app.get("/a",(req,res)=>{
+    // res.send(req.query)
+    console.log(req.query)
+    const user1 = new User({ name: req.query.Uname,password:req.query.Upassword });
+    user1.save()
+    
+    // ejs.renderFile("index.html",(err,str)=>{
+    //     res.send(str)
+    // })
+})
 app.listen(10232)
